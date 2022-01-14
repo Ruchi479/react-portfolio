05108e66445db9db70e4258fb4eaacb707f3d78c
Ruchi <ruchisaini.0404@gmail.com>
2022-01-15 03:27:40 +1100

    added Footer Page

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {   
+    return (
+    <div className="page-footer" id="footer">
+        <a href="https://github.com/Ruchi479"><img src="https://img.icons8.com/ios/80/000000/github.png" alt="github-logo" class="icon"/></a>
+        <a href="https://www.linkedin.com/in/ruchi-saini-007/"><img src="https://img.icons8.com/fluent/80/000000/linkedin-circled.png" alt="linkedin-logo" class="icon"/></a>
+    </div>
+    )
+}
+
+export default Footer;

c901dc01bc6c8876a2d9bb95088d95c381368600
Ruchi <ruchisaini.0404@gmail.com>
2022-01-15 03:40:35 +1100

    working on child components

---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Portfolio() {
+  return (
+    <div>
+      <h1>Portfolio</h1>
+      <p>
+        Integer cursus bibendum sem non pretium.
+      </p>
+    </div>
+  );
+}

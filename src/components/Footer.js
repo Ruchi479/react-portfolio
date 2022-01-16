ebb16ad325d680f9b8cc9a5ae4ce81ca969de95b
Ruchi <ruchisaini.0404@gmail.com>
2022-01-16 23:31:31 +1100

    updated footerpage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import '../../styles/Aboutme.css';
+
+const styles ={
+    footerPage: {
+        background: '#c1c1c1',
+        padding: '70px 0 50px 0',
+        position: 'relative',
+    },
+}
 
 function Footer() {   
     return (
-    <div className="page-footer" id="footer">
-        <a href="https://github.com/Ruchi479"><img src="https://img.icons8.com/ios/80/000000/github.png" alt="github-logo" class="icon"/></a>
-        <a href="https://www.linkedin.com/in/ruchi-saini-007/"><img src="https://img.icons8.com/fluent/80/000000/linkedin-circled.png" alt="linkedin-logo" class="icon"/></a>
+    // <div className="page-footer" id="footer">
+    //     <a href="https://github.com/Ruchi479"><img src="https://img.icons8.com/ios/80/000000/github.png" alt="github-logo" class="icon"/></a>
+    //     <a href="https://www.linkedin.com/in/ruchi-saini-007/"><img src="https://img.icons8.com/fluent/80/000000/linkedin-circled.png" alt="linkedin-logo" class="icon"/></a>
+    // </div>
+
+    // <a href ="#homeP"><i class="fas fa-angle-double-up arrow pagelink"></i></a>
+
+
+    <div className="icon-wrap flex row footer" style={styles.footerPage}>
+        <a href="https://www.linkedin.com/in/ruchi-saini-007/" target="_blank">
+            <div className="flex icon" id="icon-2">
+                <FontAwesomeIcon icon={faLinkedin}/>
+            </div>
+        </a>
+        <a href="https://github.com/Ruchi479" target="_blank">
+            <div className="flex icon" id="icon-3">
+                <FontAwesomeIcon icon={faGithubSquare}/>
+            </div>
+        </a>
     </div>
     )
 }
--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -22,58 +22,63 @@ const styles = {
     color:'white',
     display: 'flex',
     justifyContent: 'flex-end',
-}
+  },
+
+  navanchorStyle:{
+    color: 'green',
+    textDecoration:'none',
+    padding: '8px 25px',
+    transition: 'all 0.5s ease',
+  }
 }
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
 function NavTabs({ currentPage, handlePageChange }) {
   return (
     <nav style = {styles.navbarStyle} className="navbar">
-      <nav style = {styles.navitemStyle}>
-        <ul className="nav nav-tabs" >
-          <li className="nav-item">
-            <a 
-              href="#About"
-              onClick={() => handlePageChange('About')}
-              // This is a conditional (ternary) operator that checks to see if the current page is "About"
-              // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-              className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-            >
-              About
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              href="#Portfolio"
-              onClick={() => handlePageChange('Portfolio')}
-              // Check to see if the currentPage is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-              className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-            >
-              Portfolio
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              href="#Contact"
-              onClick={() => handlePageChange('Contact')}
-              // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-              className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-            >
-              Contact
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              href="#Resume"
-              onClick={() => handlePageChange('Resume')}
-              // Check to see if the currentPage is `Resume`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-              className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-            >
-              Resume
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <ul className="nav nav-tabs menu" style = {styles.navitemStyle}>
+        <li className="nav-item">
+          <a 
+            href="#About" style = {styles.navanchorStyle}
+            onClick={() => handlePageChange('About')}
+            // This is a conditional (ternary) operator that checks to see if the current page is "About"
+            // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+            className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+          >
+            About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#Portfolio" style = {styles.navanchorStyle} 
+            onClick={() => handlePageChange('Portfolio')}
+            // Check to see if the currentPage is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+            className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+          >
+            Portfolio
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#Contact" style = {styles.navanchorStyle} 
+            onClick={() => handlePageChange('Contact')}
+            // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+            className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+          >
+            Contact
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#Resume" style = {styles.navanchorStyle} 
+            onClick={() => handlePageChange('Resume')}
+            // Check to see if the currentPage is `Resume`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+            className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+          >
+            Resume
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 }

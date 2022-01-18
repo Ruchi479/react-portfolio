1e08039001511ef2f50e260abfcab7125235ab48
Ruchi <ruchisaini.0404@gmail.com>
2022-01-18 21:17:13 +1100

    Updates

---
+++ precache-manifest.0b54d88c0aab66a7c11a137352575301.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4812fcd703d69f44c414e0e64a4b4a9",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "70eec8eca67bf6fd0c2e",
+    "url": "/react-portfolio/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "8068e5fb05b283523554",
+    "url": "/react-portfolio/static/css/main.2a97bcfd.chunk.css"
+  },
+  {
+    "revision": "70eec8eca67bf6fd0c2e",
+    "url": "/react-portfolio/static/js/2.ec723174.chunk.js"
+  },
+  {
+    "revision": "6783895b3ff5c7af8596",
+    "url": "/react-portfolio/static/js/3.0afa8312.chunk.js"
+  },
+  {
+    "revision": "8068e5fb05b283523554",
+    "url": "/react-portfolio/static/js/main.e4ae7f7b.chunk.js"
+  },
+  {
+    "revision": "83c4f9f41df10ff64e4c",
+    "url": "/react-portfolio/static/js/runtime~main.2c192152.js"
+  },
+  {
+    "revision": "bbc0c5dbbc471a3bf036ed0d14ecb996",
+    "url": "/react-portfolio/static/media/Ruchi_Resume.bbc0c5db.pdf"
+  },
+  {
+    "revision": "16c0437412895c8ee6965e34a1cc6756",
+    "url": "/react-portfolio/static/media/cover-image.16c04374.jpg"
+  },
+  {
+    "revision": "7607273bdb0e9e7dbcaa2360010fd025",
+    "url": "/react-portfolio/static/media/img1.7607273b.jpg"
+  }
+]);

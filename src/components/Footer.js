7292515585c9f71c6a6e60c172b0486c60675ebd
Ruchi <ruchisaini.0404@gmail.com>
2022-01-18 20:55:07 +1100

    fixed bugs in background and updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,55 +1,25 @@
-import React from 'react';
-import '../styles/Footer.css';
-
-
-const styles ={
-    footerPage: {
-        background: '#c1c1c1',
-        padding: '30px 0 15px 0',
-        position: 'relative',
-        // display:'block',
-        // flexDirection:'row',
-        display:'flex',
-        justifyContent:'center',
+import React from "react";
+const styles = {
+    icon: {
+        width: 80,
+        height: 80,
+        margin: 50,
+        cursor: "pointer",
     },
-
-    footerIcon: {
-        background: '#262f38',
-        cursor: 'pointer',
-        fontSize:'18pt',
-        height: '50px',
-        margin: '0 15px',
-        overflow:'hidden',
-        position: 'relative',
-        textAlign: 'center',
-        width:'45px',
-        transition: 'background 0.3s, color 0.3s, box-shadow 0.3s, transform 0.3s',
-        display:'flex',
-        flexDirection:'row',
-        justifyContent:'center',
-    },
-}
-
-export default function Footer() {   
+};
+export default function Footer() {
     return (
-    // <div className="page-footer" id="footer">
-    //     <a href="https://github.com/Ruchi479"><img src="https://img.icons8.com/ios/80/000000/github.png" alt="github-logo" class="icon"/></a>
-    //     <a href="https://www.linkedin.com/in/ruchi-saini-007/"><img src="https://img.icons8.com/fluent/80/000000/linkedin-circled.png" alt="linkedin-logo" class="icon"/></a>
-    // </div>
-
-    // <a href ="#homeP"><i class="fas fa-angle-double-up arrow pagelink"></i></a>
-
-
-        <div className="footerPage" style={styles.footerPage}>
-            <div className='row'>
-                <a href="https://www.linkedin.com/in/ruchi-saini-007/" target="_blank">
-                    <img src={require("./assets/linkedin.png")} alt="github" className="footerIcon" style={styles.footerIcon}/>
-                </a>
-                
-                <a href="https://github.com/Ruchi479" target="_blank">
-                    <img src={require("./assets/github.png")} alt="github" className="footerIcon" style={styles.footerIcon}/>
-                </a>
+        <footer className="footer">
+            <div>
+                <div>
+                    <a href="https://github.com/Ruchi479">
+                        <img style={styles.icon} src={require("./assets/github.png")} alt="github" className="icon"></img>
+                    </a>
+                    <a href="https://www.linkedin.com/in/ruchi-saini-007/">
+                        <img style={styles.icon} src={require("./assets/linkedin.png")} alt="linkedin" className="icon"></img>
+                    </a>
+                </div>
             </div>
-        </div>
+        </footer>
     );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,46 @@
 import React from "react";
-import img2 from "./assets/img2.jpg";
+import img2 from "./assets/cover-image.jpg";
 
 const styles ={
     homeStyle: {
         display: 'flex',
         flexDirection: 'column',
-        height: '500px',
+        height: '600px',
+        width: '100%',
         justifyContent:'center',
         alignItems:'center',
         color:'white',
-        backgroundColor: '#5f5630',
-    //   background: 'url("../assets/img2.jpg") no-repeat center',
+        // backgroundColor: '#5f5630',
+        // background: '{img2} no-repeat center',
     },
-    main:{
+    mainStyle:{
         fontSize: '75px',
         fontWeight: '600',
+        top:'10%',
     },
-    text:{
-        color: 'yellow',
+    text1Style:{
+        color: 'green',
+        textShadow: '2px 2px 5px maroon',
     },
+    
 
   }
 
   export default function Header() {
   return (
-    <div className="home" id="container" style={styles.homeStyle}>
-        <div className="main" style={styles.mainStyle}>
-            {/* <img src={img2} alt="unavailable"/> */}
-            <h1 >Hi, My name is Ruchi</h1>
-            <h2 className="text" style={styles.textStyle}>I am a Full Stack Web Developer</h2>
+    
+        <div style={{ backgroundImage: 'url("https://images.unsplash.com/photo-1580927752452-89d86da3fa0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y29kaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60")', backgroundRepeat:'no-repeat', backgroundSize:'cover',}}>
+            {/* style={{backgroundImage: `url(${background})` */}
+            <div className="home" id="container" style={styles.homeStyle}>
+                <div className="main" style={styles.mainStyle}>
+                    
+                    {/* <img src={img2} alt="unavailable"/>  */}
+                        <h1 className="text1" style={styles.text1Style} >Hi, My name is Ruchi</h1>
+                    
+                        <h2 className="text1" style={styles.text1Style}>I am a Full Stack Web Developer</h2>
+                </div>
+            </div>
         </div>
-    </div>
   )
 }
 
